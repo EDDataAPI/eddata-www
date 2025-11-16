331e29e2bacda89dc510c5b2826dc122a39af2db
gOOvER <info@goover.de>
2025-11-16 11:48:36 +0100

    Fix React Hook useEffect dependency warning in commodities page

--- pages/commodities/[[...slug]].js
+++ pages/commodities/[[...slug]].js
@@ -49,6 +49,7 @@ export default function Page(props) {
       setCategories(categories_)
       playLoadingSound()
     })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.query])
 
   return (
